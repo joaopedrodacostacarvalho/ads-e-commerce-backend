3a6422791eabcdc6896bdf676d8d971977ddc328
jpcc99 <jpcc.dev@gmail.com>
2025-10-31 21:17:30 -0300

    Add id para produto

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,10 +1,11 @@
 import { Category } from "src/category/category.entity";
 
 export class Product {
-    name: string;
-    description: string;
-    price: number;
-    category: Category[];
-    isActive: boolean;
-    image: string;
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: Category[];
+  isActive: boolean;
+  image: string;
 }
