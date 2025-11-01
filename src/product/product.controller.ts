4f5893939e629a9569644c7992de931a7a884598
jpcc99 <jpcc.dev@gmail.com>
2025-10-31 21:24:33 -0300

    Fix typo

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -4,30 +4,30 @@ import { Product } from './product.entity';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productServie: ProductService) { }
+  constructor(private readonly productService: ProductService) { }
 
   @Get()
   findAll(): Product[] {
-    return this.productServie.findAll();
+    return this.productService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string): Product {
-    return this.productServie.findOne(Number(id));
+    return this.productService.findOne(Number(id));
   }
 
   @Post()
   create(@Body() product: Product) {
-    this.productServie.create(product);
+    this.productService.create(product);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updatedProduct: Product) {
-    this.productServie.update(Number(id), updatedProduct);
+    this.productService.update(Number(id), updatedProduct);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    this.productServie.remove(Number(id));
+    this.productService.remove(Number(id));
   }
 }
