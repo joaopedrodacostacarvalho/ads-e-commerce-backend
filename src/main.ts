9ed6cdd38d9a4eacdb83dc54f680b7af4c5204b2
jpcc99 <jpcc.dev@gmail.com>
2025-11-07 11:59:31 -0300

    Add typeorm e cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
-  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
