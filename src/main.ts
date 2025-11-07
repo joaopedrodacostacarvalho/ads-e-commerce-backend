1a83b00b1047469ff1c3ec4abd512cac0f2cb3cf
jpcc99 <jpcc.dev@gmail.com>
2025-11-07 17:14:48 -0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       forbidNonWhitelisted: false,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Ads E-commerce Backend')
+    .setDescription('Um projeto da cadeira de web III')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
