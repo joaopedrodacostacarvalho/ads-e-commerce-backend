e4b669e9863391681f31aecf8bf6ede4be491516
jpcc99 <jpcc.dev@gmail.com>
2025-11-07 11:58:29 -0300

    Fix atulizar o nome da categoria no dto

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
+import { IsBoolean, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+  @IsOptional()
+  @IsString({ message: 'O nome deve ser uma string' })
+  @MaxLength(100, { message: 'O nome pode ter no máximo 100 caracteres' })
+  name?: string;
+
   @IsOptional()
   @IsBoolean({ message: 'O status ativo deve ser um booleano' })
   isActive?: boolean;
