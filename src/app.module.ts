5a9bf7b4459d046c8cef988830fdf270b354c262
jpcc99 <jpcc.dev@gmail.com>
2025-11-07 12:01:16 -0300

    Adds config para a api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,25 @@ import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { ClientModule } from './client/client.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, CategoryModule, ClientModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'ads_e_commerce',
+      username: 'testuser',
+      password: 'testuser123',
+      entities: [__dirname + '/**/*.entity{.ts,.js'],
+      synchronize: true,
+    }),
+    ProductModule,
+    CategoryModule,
+    ClientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
