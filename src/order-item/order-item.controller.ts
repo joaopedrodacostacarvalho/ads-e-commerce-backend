4d87db188fd5ceb67a5cf4485125016be21c4335
jpcc99 <jpcc.dev@gmail.com>
2025-11-07 18:36:17 -0300

    Add swagger para pedido e item-pedido

--- src/order-item/order-item.controller.ts
+++ src/order-item/order-item.controller.ts
@@ -14,6 +14,7 @@ import { OrderService } from 'src/order/order.service';
 import { CreateOrderItemDto } from './dto/create-order-item.dto';
 import { OrderItem } from './entities/order-item.entity';
 import { OrderItemService } from './order-item.services';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger'; 
 
 @Controller('order/:orderId/item') // Rota aninhada
 export class OrderItemController {
@@ -25,6 +26,23 @@ export class OrderItemController {
   // POST /order/:orderId/item: Adiciona um item ao pedido
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Adiciona um novo item ao pedido especificado' }) 
+  @ApiResponse({ 
+    status: 201, 
+    description: 'Item adicionado com sucesso.', 
+    type: OrderItem 
+  }) 
+  @ApiResponse({ 
+    status: 400, 
+    description: 'Não é possível adicionar itens a um pedido não-aberto ou sem pagamento.' 
+  })
+  @ApiResponse({ status: 404, description: 'Pedido ou Produto não encontrado.' })
+  @ApiParam({ 
+    name: 'orderId', 
+    description: 'ID do pedido ao qual o item será adicionado', 
+    type: Number 
+  }) 
+  @ApiBody({ type: CreateOrderItemDto }) 
   async create(
     @Param('orderId', ParseIntPipe) orderId: number,
     @Body() createOrderItemDto: CreateOrderItemDto,
@@ -53,6 +71,23 @@ export class OrderItemController {
 
   @Delete(':itemId')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Remove um item específico de um pedido' }) 
+  @ApiResponse({ status: 204, description: 'Item removido com sucesso.' }) 
+  @ApiResponse({ 
+    status: 400, 
+    description: 'Não é possível remover itens de um pedido não-aberto ou sem pagamento.' 
+  })
+  @ApiResponse({ status: 404, description: 'Pedido ou Item do Pedido não encontrado.' })
+  @ApiParam({ 
+    name: 'orderId', 
+    description: 'ID do pedido do qual o item será removido', 
+    type: Number 
+  }) 
+  @ApiParam({ 
+    name: 'itemId', 
+    description: 'ID do item de pedido a ser removido', 
+    type: Number 
+  }) 
   async remove(
     @Param('orderId', ParseIntPipe) orderId: number,
     @Param('itemId', ParseIntPipe) itemId: number,
--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsNotEmpty, IsPositive } from 'class-validator';
 
 export class CreateOrderDto {
+  @ApiProperty({ description: 'ID do cliente que está criando o pedido', example: 5, type: Number })
   @IsNotEmpty({ message: 'O ID do cliente é obrigatório' })
   @Type(() => Number)
   @IsInt({ message: 'O ID do cliente deve ser um número inteiro' })
