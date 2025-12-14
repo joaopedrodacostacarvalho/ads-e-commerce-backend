5afa109f402397f52c16881356fbbb1c5dffa303
jpcc99 <jpcc.dev@gmail.com>
2025-12-14 14:58:30 -0300

    refactor: login e cadastro com zod

--- ads-e-commerce-frontend-mui/app/(auth)/cadastro/page.tsx
+++ ads-e-commerce-frontend-mui/app/(auth)/cadastro/page.tsx
@@ -1,82 +1,51 @@
 "use client"
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useActionState, useState } from "react";
+import { cadastro } from "../actions";
 
-export default function LoginForm() {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [role, setRole] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
-
-  const BASE_URL = "http://localhost:3000"
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // Erro se algum estiver vazio 
-    if (email.trim() == "" || password.trim() == "") {
-      setError("Digite o seu email e seha, por favor!");
-      return;
-    }
-
-    setError("");
-
-    try {
-      const response = await fetch(`${BASE_URL}/auth/register`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json", },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          password: password,
-          phone: phone,
-          role: role,
-        })
-      });
-
-      if (!response.ok) {
-        throw new Error(`Erro HTTP ${response.status}`)
-      }
-
-      const result = await response.json();
-      console.log(result);
-      setSuccess("Cadastro efetuado.");
-    } catch (error) {
-      setError("Erro ao fazer cadastro.");
-      console.error(error);
-    }
-  }
+export default function CadastroForm() {
+  const [state, action, pending] = useActionState(cadastro, undefined);
 
   return (
     <section>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {success && <p style={{ color: "green" }}>{success}</p>}
-      <form onSubmit={handleSubmit}>
+      <form action={action}>
         <div>
           <label htmlFor="name">Nome</label>
-          <input id="name" name="name" type="text" placeholder="Seu nome completo" value={name} onChange={(e) => setName(e.target.value)} required />
+          <input id="name" name="name" type="text" placeholder="Seu nome completo" required />
         </div>
+        {state?.errors?.name && <p style={{ color: "red" }}>{state.errors.name}</p>}
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input id="email" name="email" type="email" placeholder="Email" required />
         </div>
+        {state?.errors?.email && <p style={{ color: "red" }}>{state.errors.email}</p>}
         <div>
           <label htmlFor="password">Senha</label>
-          <input id="password" name="password" type="password" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input id="password" name="password" type="password" placeholder="********" required />
         </div>
+        {state?.errors?.password && (
+          <div style={{ color: "red" }}>
+            <p>A senha deve ter:</p>
+            <ul>
+              {state.errors.password.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <div>
           <label htmlFor="phone">Telefone</label>
-          <input id="phone" name="phone" type="tel" placeholder="81 9999-9999" value={phone} onChange={(e) => setPhone(e.target.value)} />
+          <input id="phone" name="phone" type="tel" placeholder="81 9999-9999" />
         </div>
+        {state?.errors?.phone && <p style={{ color: "red" }}>{state.errors.phone}</p>}
         <div>
-          <input id="role-vendedor" name="role" type="radio" value="vendedor" onChange={(e) => setRole(e.target.value)} required />
+          <input id="role-vendedor" name="role" type="radio" value="vendedor" required />
           <label htmlFor="role">Vendedor</label>
-          <input id="role-cliente" name="role" type="radio" value="consumidor" onChange={(e) => setRole(e.target.value)} required />
+          <input id="role-cliente" name="role" type="radio" value="consumidor" required />
           <label htmlFor="role">Cliente</label>
+          {state?.errors?.role && <p style={{ color: "red" }}>{state.errors.role}</p>}
         </div>
-        <button type="submit">Login</button>
+        <button disabled={pending} type="submit" >Cadastrar</button>
       </form>
       <div>
         <p>Já possui uma conta? <a href="/login">Faça o login</a> </p>
--- ads-e-commerce-frontend-mui/app/(auth)/login/page.tsx
+++ ads-e-commerce-frontend-mui/app/(auth)/login/page.tsx
@@ -1,62 +1,34 @@
 "use client"
 
-import { FormEvent, useState } from "react";
+import { useActionState } from "react";
+import { login } from "../actions";
 
 export default function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
-
-  const BASE_URL = "http://localhost:3000"
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // Erro se algum estiver vazio 
-    if (email.trim() == "" || password.trim() == "") {
-      setError("Digite o seu email e seha, por favor!");
-      return;
-    }
-
-    setError("");
-
-    try {
-      const response = await fetch(`${BASE_URL}/auth/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json", },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        })
-      });
-
-      if (!response.ok) {
-        throw new Error(`Erro HTTP ${response.status}`)
-      }
-
-      const result = await response.json();
-      console.log(result);
-      setSuccess("Login efetuado.");
-    } catch (error) {
-      setError("Erro ao fazer login.");
-      console.error(error);
-    }
-  }
+  const [state, action, pending] = useActionState(login, undefined);
 
   return (
     <section>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {success && <p style={{ color: "green" }}>{success}</p>}
-      <form onSubmit={handleSubmit}>
+      <form action={action}>
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input id="email" name="email" type="email" placeholder="Email" required />
         </div>
+        {state?.errors?.email && <p style={{ color: "red" }}>{state.errors.email}</p>}
         <div>
           <label htmlFor="password">Senha</label>
-          <input id="password" name="password" type="password" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input id="password" name="password" type="password" placeholder="********" required />
         </div>
-        <button type="submit">Login</button>
+        {state?.errors?.password && (
+          <div style={{ color: "red" }}>
+            <p>A senha deve ter:</p>
+            <ul>
+              {state.errors.password.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <button disabled={pending} type="submit" >Fazer Login</button>
       </form>
       <div>
         <p>Ainda não tem uma conta? <a href="/cadastro">Cadastre-se</a> </p>
