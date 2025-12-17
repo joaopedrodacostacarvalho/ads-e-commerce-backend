72c948ff9d5bb04ad4f5ec2d35e2e730b4432dbd
jpcc99 <jpcc.dev@gmail.com>
2025-12-17 16:09:50 -0300

    Fix: adicionar ao carrinho não funciona mais para vendedor

--- src/app/components/cartComponents/addToCartButton.tsx
+++ src/app/components/cartComponents/addToCartButton.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from '@mui/material';
 import { useCart } from '../../context/_CartContext';
+import { useAuth } from '../../(rotas)/(auth)/_AuthContext';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 // import { useCart } from '@/app/context/_CartContext';
 
 type Props = {
@@ -9,11 +12,25 @@ type Props = {
 };
 
 export default function AddToCartButton({ productId }: Props) {
+  const router = useRouter();
+  const { user } = useAuth();
   // const { addItem, loading } = useCart();
   const { cart, addItem, loading } = useCart();
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    if (user && user.role === "consumidor") {
+      setIsLogged(true);
+    }
+  }, [user]);
+
 
   const handleAdd = async () => {
-    await addItem(productId, 1); // adiciona 1 unidade
+    if (isLogged) {
+      await addItem(productId, 1); // adiciona 1 unidade
+    } else {
+      router.push("/login");
+    }
   };
 
   return (
@@ -22,8 +39,8 @@ export default function AddToCartButton({ productId }: Props) {
       variant="contained"
       // color="primary"
       onClick={handleAdd}
-      disabled={loading}
-      sx={{ mt: 1 , background: '#FFE600', color: 'black'}}
+      disabled={loading || user.role === "vendedor"}
+      sx={{ mt: 1, background: '#FFE600', color: 'black' }}
     >
       {loading ? 'Adicionando...' : 'Adicionar ao carrinho'}
     </Button>
